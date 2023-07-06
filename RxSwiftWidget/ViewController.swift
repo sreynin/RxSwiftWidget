e028442c7ed11b1967e1b17bcd2d83684c6d948c
sreinin <bosreynin7@gmail.com>
2023-07-06 09:56:17 +0900

    init Project

---
+++ RxSwiftWidget/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSwiftWidget
+//
+//  Created by Sreynin on 2023/07/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
